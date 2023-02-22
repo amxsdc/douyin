e41c70d6aad1f2dcf894b4fe4f5caf0645a958da
amxsdc <986280672@qq.com>
2023-02-22 21:19:47 +0800

    新增查询粉丝模块

--- src/service/favoriteService.go
+++ src/service/favoriteService.go
@@ -4,6 +4,7 @@ import (
 	"douyin/src/common"
 	"douyin/src/dao"
 	"douyin/src/model"
+	"github.com/jinzhu/gorm"
 	"strconv"
 )
 
@@ -165,4 +166,18 @@ func GetVideoByUserId(userId string) ([]model.Video, error) {
 	}
 
 	return videos, nil
+}
+
+// CheckFavorite 查询某用户是否点赞某视频
+func CheckFavorite(uid uint, vid uint) bool {
+	var total int
+	if err := dao.SqlSession.Table("favorites").
+		Where("user_id = ? AND video_id = ? AND state = 1", uid, vid).Count(&total).
+		Error; gorm.IsRecordNotFoundError(err) { //没有该条记录
+		return false
+	}
+	if total == 0 {
+		return false
+	}
+	return true
 }
